0baa5c097b9953e937b2766ea446a8cc01553cb9
Lucas Sass <lucasrsass@gmail.com>
2024-07-01 15:26:31 -0700

    Updated aneroid.js

--- aneroid.js
+++ aneroid.js
@@ -1,32 +1,24 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-const bodyParser = require('body-parser');
+const WebSocket = require('ws');
 
-const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
+const wss = new WebSocket.Server({ port: 4000 });
 
-app.use(bodyParser.json());
-
-io.on('connection', (socket) => {
+wss.on('connection', (ws) => {
   console.log('New client connected');
-  socket.on('disconnect', () => {
+
+  ws.on('message', (message) => {
+    console.log('Received:', message);
+    ws.send(`Server received: ${message}`);
+  });
+
+  ws.on('close', () => {
     console.log('Client disconnected');
   });
+
+  ws.on('error', (error) => {
+    console.error('WebSocket error:', error);
+  });
+
+  ws.send('Welcome to the WebSocket server!');
 });
 
-app.post('/data', (req, res) => {
-  const { device, value } = req.body;
-  console.log(`Data received from ${device}: ${value}`);
-
-  // Emit data to connected clients
-  io.emit('data-update', req.body);
-
-  res.status(200).send('Data received');
-});
-
-const PORT = 4000;
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+console.log('WebSocket server running on port 4000');
